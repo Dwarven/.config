7b38aab677dea0d7ddcdd0adb00c42594645e189
Dwarven YANG <prison.yang@gmail.com>
2019-07-17 14:57:21 +0800

    polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+for i in $(polybar -m | awk -F: '{print $1}'); do
+    MONITOR=$i polybar top & 
+done
